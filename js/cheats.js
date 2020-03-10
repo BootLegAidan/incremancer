fc775014dbe5e79e0e029cf818ddc566800aa725
xDERPYxCREEPERx <30330512+xDERPYxCREEPERx@users.noreply.github.com>
2020-03-10 12:54:01 -0500

    Update cheats.js

--- js/cheats.js
+++ js/cheats.js
@@ -2,6 +2,6 @@ var model = GameModel;
 var cheats = document.getElementById('cheats');
 
 cheats.getElementsByTagName('button')[0].onclick = function(){
-  this.model.addBlood(1000000)
+//  this.model.addBlood(1000000)
   console.log("Added Blood")
-}
+};
